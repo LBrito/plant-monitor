5016514e2f79dff59364440a0719d6a446bfc24d
Luis Brito <lbritomz@gmail.com>
2022-10-19 11:58:24 -0400

    Updates light schedule

--- light_schedule.py
+++ light_schedule.py
@@ -8,7 +8,7 @@ light = PlantSpectrum()
 def checkLightSchedule():
     now = datetime.now().time()
     today6am = now.replace(hour=7, minute=0, second=0, microsecond=0)
-    today5pm = now.replace(hour=16, minute=30, second=0, microsecond=0)
+    today5pm = now.replace(hour=17, minute=0, second=0, microsecond=0)
     print("Current Time =", now)
     if((now > today6am and now < today5pm)):
         if not light.isOn():
