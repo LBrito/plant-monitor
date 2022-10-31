a923c16e5688f45aaf4787c0f7ce648fab18019d
Luis Brito <luis.brito@thoughtworks.com>
2022-10-31 12:06:57 -0400

    Adds sensor data using pub sub

--- web/src/App.js
+++ web/src/App.js
@@ -1,29 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
-import { Amplify } from 'aws-amplify';
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
+
+import Sensors from './components/sensorData';
+
+import { Amplify, Auth } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
+
+Auth.currentCredentials().then(creds => console.log(creds));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container className="p-4">
+        <Row className="p-3 justify-content-md-center">
+          <Col md="auto"> <Sensors name="relativeTemperature" unit="°C" /> </Col>
+          <Col md="auto"> <Sensors name="relativeHumidity" unit="%" /> </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, true);
